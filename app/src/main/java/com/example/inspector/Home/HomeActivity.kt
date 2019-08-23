ba87148b9dd01d0a9e1e22b4cd766d405a5d26c2
Lucas Alves dos Santos <lucas.alves0828@gmailcom>
2019-08-22 23:05:33 -0300

    Layout do Login

---
+++ app/src/main/java/com/example/inspector/Home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.inspector.Home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.inspector.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/example/inspector/Login/LoginContract.kt
@@ -0,0 +1,9 @@
+package com.example.inspector.Login
+
+/**
+ * Created by Lucas Alves dos Santos on 22/08/2019.
+ * lucas.alves0828@gmail.com
+ * {@see more in https://github.com/lucasalves08}
+ */
+class LoginContract {
+}
