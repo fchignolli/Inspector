8138adac834b905b890bb494a100c9b848b3c5d6
Lucas Alves dos Santos <lucas.alves0828@gmailcom>
2020-05-07 20:32:51 -0300

    Added Register and Forgot Password Views

---
+++ app/src/main/java/com/example/inspector/View/ForgotPassowordActivity.kt
@@ -0,0 +1,13 @@
+package com.example.inspector.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.inspector.R
+
+class ForgotPassowordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_passoword)
+    }
+}
