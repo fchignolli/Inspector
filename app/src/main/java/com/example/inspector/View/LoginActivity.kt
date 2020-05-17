ad3a101135cb50740c77d628f25e20eb0f75732a
Lucas Alves dos Santos <lucas.alves0828@gmailcom>
2020-05-17 17:31:15 -0300

    Added click in register and forget password

--- app/src/main/java/com/example/inspector/View/LoginActivity.kt
+++ app/src/main/java/com/example/inspector/View/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.example.inspector.View
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.inspector.R
 
 class LoginActivity : AppCompatActivity() {
@@ -10,4 +12,15 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+    fun openRegister(view: View) {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun openForgotPassword(view: View) {
+        val intent = Intent(this, ForgotPassowordActivity::class.java)
+        startActivity(intent)
+    }
+
 }
