bf63441eb3152d89ba3587cfd88a3fac0522a8d3
Lucas Alves dos Santos <lucas.alves0828@gmailcom>
2020-05-20 00:36:24 -0300

    Added singOut method

--- app/src/main/java/com/example/inspector/Controller/HomeActivity.kt
+++ app/src/main/java/com/example/inspector/Controller/HomeActivity.kt
@@ -1,13 +1,37 @@
 package com.example.inspector.Controller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.inspector.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private val TAG = "LoginActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        updateUI(auth.currentUser)
+    }
+
+    fun onClickSingOut(view: View) {
+        auth.signOut()
+        updateUI(auth.currentUser)
+    }
+
+    fun updateUI(currentUser: FirebaseUser?) {
+        if(currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
