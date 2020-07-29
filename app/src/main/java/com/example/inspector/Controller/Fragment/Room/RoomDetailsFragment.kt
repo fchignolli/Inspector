df7385020812e42e1ec559f626dcc0f81bf72379
Lucas Alves dos Santos <lucas.alves0828@gmailcom>
2020-07-29 07:28:28 -0300

    Added Tool and Room Details

--- app/src/main/java/com/example/inspector/Controller/Fragment/Room/RoomDetailsFragment.kt
+++ app/src/main/java/com/example/inspector/Controller/Fragment/Room/RoomDetailsFragment.kt
@@ -5,6 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.inspector.Controller.Adapter.FormImageAdapter
+import com.example.inspector.Model.Control
+import com.example.inspector.Model.Room
 import com.example.inspector.R
 
 /**
@@ -14,12 +21,124 @@ import com.example.inspector.R
  */
 class RoomDetailsFragment : Fragment() {
     private val TAG = "RoomDetailsFragment"
+    private lateinit var dateEditText: EditText
+    private lateinit var placeNameEditText: EditText
+    private lateinit var nameEditText: EditText
+    private lateinit var securityLightCheckBox: CheckBox
+    private lateinit var electronicDeviceCheckBox: CheckBox
+    private lateinit var communicateDeviceCheckBox: CheckBox
+    private lateinit var openDeviceCheckBox: CheckBox
+    private lateinit var openExternalDeviceCheckBox: CheckBox
+    private lateinit var lightingDeviceCheckBox: CheckBox
+    private lateinit var buttonsCheckBox: CheckBox
+    private lateinit var symbolsCheckBox: CheckBox
+    private lateinit var informationCheckbox: CheckBox
+    private lateinit var actionPlanCheckBox: CheckBox
+    private lateinit var observationEditText: EditText
+    private lateinit var imageRecyclerView: RecyclerView
+
+    private lateinit var room: Room
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        room = arguments?.getSerializable("room") as Room
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_room_details, container, false)
+        val root = inflater.inflate(R.layout.fragment_room_details, container, false)
+        dateEditText = root.findViewById(R.id.dateRoomDetEditText)
+        placeNameEditText = root.findViewById(R.id.placeNameRoomDetEditText)
+        nameEditText = root.findViewById(R.id.nameRoomDetEditText)
+        securityLightCheckBox = root.findViewById(R.id.securityLightRoomDetCheckBox)
+        electronicDeviceCheckBox = root.findViewById(R.id.electronicDeviceRoomDetCheckBox)
+        communicateDeviceCheckBox = root.findViewById(R.id.communicateDeviceRoomDetCheckBox)
+        openDeviceCheckBox = root.findViewById(R.id.openDeviceRoomDetCheckBox)
+        openExternalDeviceCheckBox = root.findViewById(R.id.openExtDeviceRoomDetCheckBox)
+        lightingDeviceCheckBox = root.findViewById(R.id.lighthingDeviceRoomDetCheckBox)
+        buttonsCheckBox = root.findViewById(R.id.ButtonsShouldRoomDetCheckBox)
+        symbolsCheckBox = root.findViewById(R.id.symbolsRoomDetCheckBox)
+        informationCheckbox = root.findViewById(R.id.infoRoomDetCheckBox)
+        actionPlanCheckBox = root.findViewById(R.id.actionPlanRoomDetCheckBox)
+        observationEditText = root.findViewById(R.id.observationRoomDetEditText)
+        imageRecyclerView = root.findViewById(R.id.imageRoomDetRecyclerView)
+        return root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setupUI()
+    }
+
+    private fun setupUI() {
+        val editTexts = getArrayOfEditTexts()
+        val checkBoxes = getArrayOfCheckBoxes()
+        configureEditTexts(editTexts, false)
+        configureCheckBoxes(checkBoxes, false)
+        setValuesOnView()
+        setupImageRecyclerView()
+    }
+
+    private fun setupImageRecyclerView() {
+        imageRecyclerView.layoutManager = GridLayoutManager(context, 2)
+        imageRecyclerView.adapter = FormImageAdapter(room.images, ArrayList(), context, canRemove = false)
+        imageRecyclerView.setHasFixedSize(true)
+    }
+
+    private fun setValuesOnView() {
+        val date = "${room.getUserDate()}  ${room.getUserTime()}"
+        dateEditText.setText(date)
+        placeNameEditText.setText(room.placeName)
+        nameEditText.setText(room.inspectedName)
+        securityLightCheckBox.isChecked = room.hasSecurityLight
+        electronicDeviceCheckBox.isChecked = room.hasElectronicDevice
+        communicateDeviceCheckBox.isChecked = room.hasCommunicateDevice
+        openDeviceCheckBox.isChecked = room.hasOpenDevice
+        openExternalDeviceCheckBox.isChecked = room.hasOpenExternalDevice
+        lightingDeviceCheckBox.isChecked = room.hasLightingDevice
+        buttonsCheckBox.isChecked = room.hasButtons
+        symbolsCheckBox.isChecked = room.hasSymbol
+        informationCheckbox.isChecked = room.hasInformation
+        actionPlanCheckBox.isChecked = room.hasActionPlan
+        observationEditText.setText(room.observation)
+    }
+
+    private fun getArrayOfEditTexts(): ArrayList<EditText> {
+        return arrayListOf(
+            dateEditText,
+            placeNameEditText,
+            nameEditText,
+            observationEditText
+        )
+    }
+
+    private fun getArrayOfCheckBoxes(): ArrayList<CheckBox> {
+        return arrayListOf(
+            securityLightCheckBox,
+            electronicDeviceCheckBox,
+            communicateDeviceCheckBox,
+            openDeviceCheckBox,
+            openExternalDeviceCheckBox,
+            lightingDeviceCheckBox,
+            buttonsCheckBox,
+            symbolsCheckBox,
+            informationCheckbox,
+            actionPlanCheckBox
+        )
+    }
+
+    private fun configureCheckBoxes(checkboxes: ArrayList<CheckBox>, isEnabled: Boolean) {
+        for(checkbox in checkboxes) {
+            checkbox.isClickable = isEnabled
+        }
+    }
+
+    private fun configureEditTexts(editTexts: ArrayList<EditText>, isEnabled: Boolean) {
+        for(editText in editTexts) {
+            editText.isEnabled = isEnabled
+        }
     }
 
 }
--- app/src/main/java/com/example/inspector/Controller/Fragment/Tool/ToolDetailsFragment.kt
+++ app/src/main/java/com/example/inspector/Controller/Fragment/Tool/ToolDetailsFragment.kt
@@ -5,6 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.inspector.Controller.Adapter.FormImageAdapter
+import com.example.inspector.Model.Tool
 import com.example.inspector.R
 
 /**
@@ -13,13 +19,108 @@ import com.example.inspector.R
  * {@see more in https://github.com/lucasalves08}
  */
 class ToolDetailsFragment : Fragment() {
+    private val TAG = "RoomDetailsFragment"
+    private lateinit var dateEditText: EditText
+    private lateinit var placeNameEditText: EditText
+    private lateinit var nameEditText: EditText
+    private lateinit var barometerEditText: EditText
+    private lateinit var thermometerEditText: EditText
+    private lateinit var rulerEditText: EditText
+    private lateinit var simulatorCheckbox: CheckBox
+    private lateinit var laserCheckbox: CheckBox
+    private lateinit var stabilityCheckBox: CheckBox
+    private lateinit var observationEditText: EditText
+    private lateinit var imageRecyclerView: RecyclerView
 
+    private lateinit var tool: Tool
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        tool = arguments?.getSerializable("tool") as Tool
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tool_details, container, false)
+        val root = inflater.inflate(R.layout.fragment_tool_details, container, false)
+        dateEditText = root.findViewById(R.id.dateToolDetEditText)
+        placeNameEditText = root.findViewById(R.id.placeNameToolDetEditText)
+        nameEditText = root.findViewById(R.id.nameToolDetEditText)
+        barometerEditText = root.findViewById(R.id.barometerDetailsEditText)
+        thermometerEditText = root.findViewById(R.id.thermometerDetEditText)
+        rulerEditText = root.findViewById(R.id.rulerDetEditText)
+        simulatorCheckbox = root.findViewById(R.id.simuladorToolDetCheckbox)
+        laserCheckbox = root.findViewById(R.id.laserToolDetCheckbox)
+        stabilityCheckBox = root.findViewById(R.id.stabilityToolDetCheckBox)
+        observationEditText = root.findViewById(R.id.observationToolDetEditText)
+        imageRecyclerView = root.findViewById(R.id.imageToolDetRecyclerView)
+        return root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setupUI()
+    }
+
+    private fun setupUI() {
+        val editTexts = getArrayOfEditTexts()
+        val checkBoxes = getArrayOfCheckBoxes()
+        configureEditTexts(editTexts, false)
+        configureCheckBoxes(checkBoxes, false)
+        setValuesOnView()
+        setupImageRecyclerView()
+    }
+
+    private fun setupImageRecyclerView() {
+        imageRecyclerView.layoutManager = GridLayoutManager(context, 2)
+        imageRecyclerView.adapter = FormImageAdapter(tool.images, ArrayList(), context, canRemove = false)
+        imageRecyclerView.setHasFixedSize(true)
+    }
+
+    private fun setValuesOnView() {
+        val date = "${tool.getUserDate()}  ${tool.getUserTime()}"
+        dateEditText.setText(date)
+        placeNameEditText.setText(tool.placeName)
+        nameEditText.setText(tool.inspectedName)
+        barometerEditText.setText(tool.barometerResolution)
+        thermometerEditText.setText(tool.thermometerResolution)
+        rulerEditText.setText(tool.rulerResolution)
+        simulatorCheckbox.isChecked = tool.hasSimulatorDosimetry
+        laserCheckbox.isChecked = tool.hasLaserAligner
+        stabilityCheckBox.isChecked = tool.hasStabilityChecker
+        observationEditText.setText(tool.observation)
+    }
+
+    private fun getArrayOfEditTexts(): ArrayList<EditText> {
+        return arrayListOf(
+            dateEditText,
+            placeNameEditText,
+            nameEditText,
+            barometerEditText,
+            thermometerEditText,
+            rulerEditText,
+            observationEditText
+        )
+    }
+
+    private fun getArrayOfCheckBoxes(): ArrayList<CheckBox> {
+        return arrayListOf(
+            simulatorCheckbox,
+            laserCheckbox,
+            stabilityCheckBox
+        )
+    }
+
+    private fun configureCheckBoxes(checkboxes: ArrayList<CheckBox>, isEnabled: Boolean) {
+        for(checkbox in checkboxes) {
+            checkbox.isClickable = isEnabled
+        }
+    }
+
+    private fun configureEditTexts(editTexts: ArrayList<EditText>, isEnabled: Boolean) {
+        for(editText in editTexts) {
+            editText.isEnabled = isEnabled
+        }
     }
 
 }
